0190a6d2fcd1892ddd1711c23444fbe0478143d5
Chelsea Spangler <chelseanspangler@gmail.com>
2020-01-16 10:13:46 -0800

    functions work, results random

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
 import checkResult from './checkResult.js';
 import foeThrow from './foeThrow.js';
-import { getRandomNumber } from './foeThrow.js';
-import { win, lose, draw } from './checkResult.js';
 
 const rumble = document.getElementById('rumble');
 const reset = document.getElementById('reset');
-const player = document.querySelector('.input:checked');
 const winLose = document.getElementById('winLose');
-const computer = getRandomNumber(3);
 const playedCount = document.getElementById('playedCount');
 const winCount = document.getElementById('winCount');
 let count = 0;
 let wins = 0;
+winCount.textContent = wins;
+playedCount.textContent = count;
 
 rumble.addEventListener('click', () => {
     winLose.textContent = '';
     count++;
     playedCount.textContent = count;
+    const player = document.querySelector('input:checked').value;
     foeThrow();
+    const computer = foeThrow();
+    console.log(player);
+    console.log(computer);
     checkResult(player, computer);
-    // Need a separate isWinner function w boolean result? e.g. if isWin === true
-    if (checkResult === win()) {
+    if (checkResult() === 'draw') {
+        winLose.textContent = 'It\'s a draw!';
+    } else if (checkResult() === 'win') {
+        winLose.textContent = 'You won!';
         wins++;
+    } else if (checkResult() === 'lose') {
+        winLose.textContent = 'You lost!';
     };
     winCount.textContent = wins;
 });
@@ -29,4 +35,7 @@ rumble.addEventListener('click', () => {
 reset.addEventListener('click', () => {
     count = 0;
     wins = 0;
+    winLose.textContent = '';
+    winCount.textContent = wins;
+    playedCount.textContent = count;
 })
