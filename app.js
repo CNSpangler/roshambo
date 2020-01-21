1102863332a7a9e307ebb07a598d174f3d08cf83
Chelsea Spangler <chelseanspangler@gmail.com>
2020-01-21 10:45:17 -0800

    cleaned up styling and code; added hover effects

--- app.js
+++ app.js
@@ -1,45 +1,43 @@
 import checkResult from './checkResult.js';
 import foeThrow from './foeThrow.js';
 
+// get DOM and set up variables
 const rumble = document.getElementById('rumble');
 const reset = document.getElementById('reset');
 const winLose = document.getElementById('winLose');
 const playedCount = document.getElementById('playedCount');
 const winCount = document.getElementById('winCount');
-let count = 0;
-let wins = 0;
-winCount.textContent = wins;
-playedCount.textContent = count;
 const playerImg = document.getElementById('playerImg');
 const computerImg = document.getElementById('computerImg');
+let count = 0;
+let wins = 0;
 
+// RUMBLE button click events
 rumble.addEventListener('click', () => {
+    const player = document.querySelector('input:checked').value;
+    const computer = foeThrow();
+    const result = checkResult(player, computer);
     winLose.textContent = '';
     count++;
     playedCount.textContent = count;
-    const player = document.querySelector('input:checked').value;
-    const computer = foeThrow();
-    playerWeapon.textContent = player;
-    computerWeapon.textContent = computer;
-    const result = checkResult(player, computer);
     if (result === 'draw') {
         winLose.textContent = 'It\'s a draw!';
     } else if (result === 'win') {
         winLose.textContent = 'You won!';
         wins++;
+        winCount.textContent = wins;
     } else if (result === 'lose') {
         winLose.textContent = 'You lost!';
     }
-    winCount.textContent = wins;
 
-// show player result images instead of text
+// show player result image instead of text
     if (player === 'rock') {
         playerImg.src = './assets/rock.png';
     } else if (player === 'paper') {
         playerImg.src = './assets/paper.png';
     } else if (player === 'scissors') {
         playerImg.src = './assets/scissors.png';
-    };
+    }
 // show computer results image instead of text
     if (computer === 'rock') {
         computerImg.src = './assets/rock.png';
@@ -50,12 +48,11 @@ rumble.addEventListener('click', () => {
     }
 });
 
+// reset button events
 reset.addEventListener('click', () => {
     count = 0;
     wins = 0;
     winLose.textContent = '';
     winCount.textContent = wins;
     playedCount.textContent = count;
-    playerWeapon.textContent = '';
-    computerWeapon.textContent = '';
-})
+});
